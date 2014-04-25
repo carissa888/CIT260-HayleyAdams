f0668f95b66db41ab33f9de4d4fcd7c845206052
carissa888 <ada12025@byui.edu>
2014-04-25 15:23:13 -0600

    Create GalToLit.java

---
+++ GalToLit.java
@@ -0,0 +1,20 @@
+/**
+*Converts Gallons to Liters
+*by Hayley Adams
+*/
+
+
+
+public class GalToLit {
+
+	public static void main(String args[]) {
+		double gallons; //number of gallons
+		double liters; //number of liters
+		
+		gallons = 10;
+		liters = gallons * 3.7854; //convert to liters
+		
+		System.out.println(gallons + " gallons is " + liters + "liters.");
+	}
+	
+}	
